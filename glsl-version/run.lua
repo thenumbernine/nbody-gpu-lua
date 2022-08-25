5491fc72a664850cf1c9a20fa4f4e1ac57f263fa
thenumbernine <christopher.e.moore@gmail.com>
2022-08-25 13:11:12 +0700

    a mess but working

--- glsl-version/run.lua
+++ glsl-version/run.lua
@@ -6,37 +6,50 @@ local table = require 'ext.table'
 local class = require 'ext.class'
 local vector = require 'ffi.cpp.vector'
 local template = require 'template'
+local GLTex1D = require 'gl.tex2d'
 local GLTex2D = require 'gl.tex2d'
-local GLPixelPackBuffer = require 'gl.pixelpackbuffer'
 local GLPingPong = require 'gl.pingpong'
 local GLProgram = require 'gl.program'
+local GLArrayBuffer = require 'gl.arraybuffer'
+local GLAttribute = require 'gl.attribute'
 local GLKernelProgram = require 'gl.kernelprogram'
+local glreport = require 'gl.report'
 local gl = require 'gl'
+local clnumber = require 'cl.obj.number'	-- TODO since gl needs this too, and cl depends on gl for interop, how about move this to gl?
 local vec2i = require 'vec-ffi.vec2i'
+local vec2f = require 'vec-ffi.vec2f'
 local vec3f = require 'vec-ffi.vec3f'
 local vec4f = require 'vec-ffi.vec4f'
 
+local matrix_ffi = require 'matrix.ffi'
+matrix_ffi.real = 'float'	-- default matrix_ffi type
+
 local App = class(require 'glapp.orbit'(require 'imguiapp'))
 
-local fieldDim = 128
+local fieldDim = 64
 local count = fieldDim * fieldDim
 local update = true
 
--- TODO goes in gl.tex and needs to be public
-local ctypePerGLType = {
-	[gl.GL_UNSIGNED_BYTE] = 'uint8_t',
-	[gl.GL_BYTE] = 'int8_t',
-	[gl.GL_UNSIGNED_SHORT] = 'uint16_t',
-	[gl.GL_SHORT] = 'int16_t',
-	[gl.GL_UNSIGNED_INT] = 'uint32_t',
-	[gl.GL_INT] = 'int32_t',
-	[gl.GL_FLOAT] = 'float',
-}
 local channelsPerFormat = {
 	[gl.GL_RGB] = 3,
 	[gl.GL_RGBA] = 4,
 }
 
+local function dataFromLambda(width, height, format, gltype, callback)
+	local ctype = assert(GLTex2D.ctypeForGLType[gltype], "couldn't determine size of type "..tostring(gltype))
+	local numChannels = assert(channelsPerFormat[format], "couldn't determine channels for format "..tostring(format))
+	local ptr = ffi.new(ctype..'[?]', width * height * numChannels)
+	for j=0,height-1 do
+		for i=0,width-1 do
+			local e = i + width * j
+			local chs = table.pack(callback(i, j))
+			for ch=0,numChannels-1 do
+				ptr[ch + numChannels * e] = chs[ch+1] or 0
+			end
+		end
+	end
+	return ptr
+end
 
 local function createFieldPingPong(args)
 	args = args or {}
@@ -47,20 +60,7 @@ local function createFieldPingPong(args)
 	local internalFormat = args.internalFormat or gl.GL_RGBA32F
 	local data = args.data
 	if type(data) == 'function' then
-		local ctype = assert(ctypePerGLType[gltype], "couldn't determine size of type "..tostring(gltype))
-		local numChannels = assert(channelsPerFormat[format], "couldn't determine channels for format "..tostring(format))
-		local ptr = ffi.new(ctype..'[?]', width * height * numChannels)
-		for j=0,height-1 do
-			for i=0,width-1 do
-				local e = i + width * j
-				local chs = table.pack(data(i, j))
-				assert(#chs == numChannels)
-				for ch=0,numChannels-1 do
-					ptr[ch + numChannels * e] = chs[ch+1] or 0
-				end
-			end
-		end
-		data = ptr
+		data = dataFromLambda(width, height, format, gltype)
 	end
 	return GLPingPong{
 		width = width,
@@ -75,15 +75,75 @@ local function createFieldPingPong(args)
 end
 
 
+local uvAttr
 local posTexs
 local velTexs
-local vbo
 
+--[[
+ok for some real world values ...
+G = 6.67384e-11 m^3 / (kg s^2)
+
+using new units ...
+1 lyr = 9.4607304725808e+15 m
+1 ux = 1e+5 lyr = 9.4607304725808e+20 m		-- diameter of the milky way
+1 day = 60 * 60 * 24 s = 8.64e+4 s
+1 year = 365.25 days = 3.15576e+7 s
+1 ut = 2.3e+8 years = 7.258248e+15 s 		-- time for the solar system to orbit the galaxy
+1 um = 1 solar mass = 1.989e+30 kg
+G = 8.2584809222832e-12 ux^3 / (um ut^2)	-- using units of sun orbit distance, sun orbit time, and sun mass ... same.  I bet Kepler has a law about this.
+
+what if we use the sun's orbit distance also 
+1 ux = 2.58e+4 lyr = 2.4408684619258e+20 m		-- sun orbit distance in milky way ... https://www.universetoday.com/148997/a-new-measurement-puts-the-sun-2000-light-years-closer-to-the-center-of-the-milky-way/
+G = 4.8088480226311e-10 ux^3 / (um ut^2)	-- it gets closer to G
+sun velocity = 2.51e+5 m/s = 7.4638198510813 ux / ut
+
+how about Earth parameters?
+1 ux = 1 au = 1.495978707e+11 m				-- average distance of sun to earth
+1 ut = 1 year = 3.15576e+7 s				-- average time of earth orbit
+1 um = 5.9722e+24 kg						-- weight of Earth
+G = 1.18560994016190149433383205224146195178036577999592e-04 ux^3 / (um ut^2)	-- G in Earth orbit units
+
+how about the moon?
+1 ux = 1 ld = 3.84399e+8 m
+1 ut = 1 month = 27.321662 days = 2360591.5968 s
+1 um = 7.349e+22 kg
+G = 4.81170511702339376292059114348376169800758361816406e-1
+
+so our "gravitation parameter" G_orbit = G * mass_in_kg * orbit_time_in_s^2 / orbit_dist_in_m^3
+--]]
 local dt = .001
+--local gravConst = 4.8088480226311e-10	-- in sun units
+local gravConst = .001
 
 local function reset()
+	do
+		local width = fieldDim
+		local height = fieldDim
+		local format = gl.GL_RGBA
+		local gltype = gl.GL_FLOAT
+		uvCPUMem = dataFromLambda(width, height, format, gltype, function(i,j)
+			return (i+.5)/width, (j+.5)/height
+		end)
+		-- [[
+		uvBuf = GLArrayBuffer{
+			size = count * ffi.sizeof'vec2f_t',
+			data = uvCPUMem,
+			usage = gl.GL_STATIC_DRAW,
+		}
+		uvAttr = GLAttribute{
+			buffer = uvBuv,
+			size = 2,
+			type = gl.GL_FLOAT,
+			stride = ffi.sizeof'vec2f_t',
+			offset = 0,
+		}
+		--]]
+	end
+
 	local posData = vector('vec4f_t', count)
 	local velData = vector('vec4f_t', count)
+	local posv = ffi.cast('vec4f_t*', posData.v)	-- TODO cast within vector:begin() ?
+	local velv = ffi.cast('vec4f_t*', velData.v)	-- TODO cast within vector:begin() ?
 	for i=0,count-1 do
 --[[
 		return (u+.5)/fieldDim*2-1,(v+.5)/fieldDim*2-1, 0, 0
@@ -99,19 +159,26 @@ local function reset()
 		return v[1], v[2], v[3], 1
 --]]
 -- [[
-		local r = math.random()
+		local r = math.random() * 10
 		local phi = math.random() * 2 * math.pi
-		local z = math.exp(-r*r)
-		posData.v[i].x = math.cos(phi) * r
-		posData.v[i].y = math.sin(phi) * r
-		posData.v[i].z = z
-		posData.v[i].w = 1
+		local z = (math.random() * 2 - 1) * math.exp(-r*r)
+		posv[0].x = math.cos(phi) * r
+		posv[0].y = math.sin(phi) * r
+		posv[0].z = z
+		local mass = math.random() + .1
+		posv[0].w = mass
 --]]
 
-		velData.v[i].x = 0
-		velData.v[i].y = 0
-		velData.v[i].z = 0
-		velData.v[i].w = 0
+		-- G m_sum = omega^2 r^3
+		-- omega = angular velocity, s.t. x = r cis (omega t), v = r omega cis (omega t)
+		local omega = 1 * 2 * math.pi * math.sqrt(mass / r)
+		velv[0].x = -omega * posv[0].y
+		velv[0].y = omega * posv[0].x
+		velv[0].z = 0
+		velv[0].w = 0
+	
+		posv = posv + 1
+		velv = velv + 1
 	end
 	posTexs = createFieldPingPong{data = posData.v}
 	velTexs = createFieldPingPong{data = velData.v}
@@ -119,15 +186,33 @@ end
 
 App.viewDist = 1
 
+messageCallback = function(
+	source,		-- GLenum
+	gltype,		-- GLenum
+	id,			-- GLuint
+	severity,	-- GLenum
+	length,		-- GLsizei
+	msg,		-- GLchar const *
+	userParam	-- void const * 
+)
+	print('gl error', source, gltype, id, severity, ffi.string(msg, length))
+end
+messageCallbackClosure = ffi.cast('GLDEBUGPROC', messageCallback)
+
 function App:initGL(...)
 	App.super.initGL(self, ...)
-	
-	self.view.znear = .01
-	self.view.zfar = 10
+
+--[[ crashing
+	gl.glDebugMessageCallback(messageCallbackClosure, nil)
+	gl.glEnable(gl.GL_DEBUG_OUTPUT)
+--]]
+
+	self.view.znear = .1
+	self.view.zfar = 100
 
 	reset()
 
-	hsvtex = require 'gl.gradienttex'(256, 
+	gradTex = require 'gl.gradienttex'(256, 
 --[[ rainbow or heatmap or whatever
 		{
 			{0,0,0,0},
@@ -156,30 +241,25 @@ function App:initGL(...)
 		false
 	)
 
-	-- [[
-	vbo = GLPixelPackBuffer{size = fieldDim * fieldDim * 4 * 4, usage = gl.GL_DYNAMIC_DRAW}
-	--]]
-
 	simPosShader = GLKernelProgram{
-		texs={'postex', 'veltex'};
-		code = [[
+		texs = {'postex', 'veltex'};
+		code = template([[
 void main() {
 	vec4 vpos = texture2D(postex, pos);
 	vec4 vvel = texture2D(veltex, pos);
-	const float dt = ]]..dt..[[;
-	vpos.xyz += vvel.xyz * dt;
-	vpos.w = length(vvel);
+	vpos.xyz += vvel.xyz * <?=dt?>;
+	// preserve vpos.w == mass
 	gl_FragColor = vpos;
 }
-]];
+]],		{
+			dt = clnumber(dt),
+		}),
 	}
 
 	simVelShader = GLKernelProgram{
-		texs={'postex','veltex'};
+		texs = {'postex','veltex'};
 		code = template([[
 void main() {
-	const float dt = <?=dt?>;
-	const float gravConst = .1;
 	vec4 vpos = texture2D(postex, pos);
 	vec4 vvel = texture2D(veltex, pos);
 	vec3 accumforce = vec3(0.);
@@ -187,64 +267,122 @@ void main() {
 	for (otc.x = .5/<?=fieldDim?>; otc.x < 1.; otc.x += 1./<?=fieldDim?>) {
 		for (otc.y = .5/<?=fieldDim?>; otc.y < 1.; otc.y += 1./<?=fieldDim?>) {
 			vec4 otherpos = texture2D(postex, otc);
+			float othermass = otherpos.w;
 			vec3 del = otherpos.xyz - vpos.xyz;
 			float len = length(del);
-			len = max(len, .001);
-			del *= (gravConst / len*len*len);
+			len = max(len, .00001);
+			float invlen = 1. / len;
+			del *= invlen;
+			del *= invlen;
+			del *= invlen;
+			del *= othermass;
+			del *= <?=gravConst?>;
 			accumforce += del;
 		}
 	}
-	vvel.xyz += accumforce * dt;
+	vvel.xyz += accumforce * <?=dt?>;
 	gl_FragColor = vvel;
 }
 ]],		{
-			dt = dt,
-			fieldDim = require 'cl.obj.number'(fieldDim),
+			dt = clnumber(dt),
+			gravConst = clnumber(gravConst),
+			fieldDim = clnumber(fieldDim),
 		}),
 	}
 
 	displayShader = GLProgram{
-		uniforms={
-			tex=0,
-		};
 		vertexCode = [[
-varying float magn;
+#version 460
+
+uniform sampler2D posTex, velTex;
+uniform mat4 modelViewProjectionMatrix;
+
+in vec2 uv;
+
+out float magn;
+
 void main() {
-	vec4 v = vec4(gl_Vertex.xyz, 1.);
-	magn = gl_Vertex.w;
-	gl_Position = gl_ModelViewProjectionMatrix * v;
+	vec4 pos4 = texture(posTex, uv);
+	vec4 vel = texture(velTex, uv);
+	vec4 pos = vec4(pos4.xyz, 1.);
+	magn = length(pos.xyz);
+	gl_Position = modelViewProjectionMatrix * pos;
 }
-]];
+]],
 		fragmentCode = [[
-#define	magnscale	.01
-varying float magn;
-uniform sampler1D tex;
+#version 460
+
+#define	magnscale	.1
+
+in float magn;
+
+uniform sampler1D gradTex;
+
+out vec4 color;
+
 void main() {
-	gl_FragColor = texture1D(tex, magn * magnscale);
+	color = texture(gradTex, magn * magnscale);
 }
-]];
+]],
+		uniforms = {
+			posTex = 0,
+			velTex = 1,
+			gradTex = 2,
+		},
+		-- [[
+		attrs = {
+			uv = uvAttr,
+		},
+		--]]
+		--[[
+		attrLocs = {
+			uv = 0,
+		},
+		--]]
 	}
+	--[[
+	displayShader:setAttrs{
+		uv = uvAttr,
+	}
+	--]]
+	displayShader:useNone()
 
 	gl.glDisable(gl.GL_DEPTH_TEST)
 	gl.glDisable(gl.GL_CULL_FACE)
 end
 
 local viewport = vec4f()
+
+local modelViewMatrix = matrix_ffi.zeros(4,4)
+local projectionMatrix = matrix_ffi.zeros(4,4)
+local modelViewProjectionMatrix = matrix_ffi.zeros(4,4)
+
 function App:update(...)
+glreport'here'	
 	gl.glClear(gl.GL_COLOR_BUFFER_BIT)
+glreport'here'	
 
 
-	gl.glEnable(gl.GL_TEXTURE_2D)
+--	gl.glEnable(gl.GL_TEXTURE_2D)
 	
 	gl.glGetFloatv(gl.GL_VIEWPORT, viewport.s)
+glreport'here'	
 	gl.glViewport(0,0,fieldDim,fieldDim)
+glreport'here'	
 	gl.glMatrixMode(gl.GL_PROJECTION)
+glreport'here'	
 	gl.glPushMatrix()
+glreport'here'	
 	gl.glLoadIdentity()
+glreport'here'	
 	gl.glOrtho(0,1,0,1,-1,1)
+glreport'here'	
 	gl.glMatrixMode(gl.GL_MODELVIEW)
+glreport'here'	
 	gl.glPushMatrix()
+glreport'here'	
 	gl.glLoadIdentity()
+glreport'here'	
 	if update then
 		velTexs:swap()
 		velTexs:draw{
@@ -257,11 +395,19 @@ function App:update(...)
 			texs={posTexs:prev(), velTexs:prev()};
 		}
 	end
+glreport'here'	
 	gl.glViewport(viewport:unpack())
+glreport'here'	
 	gl.glMatrixMode(gl.GL_PROJECTION)
+glreport'here'	
 	gl.glPopMatrix()
+glreport'here'	
 	gl.glMatrixMode(gl.GL_MODELVIEW)
+glreport'here'	
 	gl.glPopMatrix()
+glreport'here'	
+
+--	gl.glDisable(gl.GL_TEXTURE_2D)
 
 --[[
 	velTexs:cur():bind()
@@ -278,27 +424,71 @@ function App:update(...)
 --]]
 
 
--- [[
-	-- copy from texture to vbo
-	gl.glBindBuffer(gl.GL_PIXEL_PACK_BUFFER, vbo.id)
-	gl.glBindTexture(gl.GL_TEXTURE_2D, posTexs:cur().id)
-	gl.glGetTexImage(gl.GL_TEXTURE_2D, 0, gl.GL_RGBA, gl.GL_FLOAT, nil)
-	gl.glBindTexture(gl.GL_TEXTURE_2D, 0)
-	gl.glBindBuffer(gl.GL_PIXEL_PACK_BUFFER, 0)
+	self.view:setup(self.width / self.height)
+	gl.glGetFloatv(gl.GL_MODELVIEW_MATRIX, modelViewMatrix.ptr)
+	gl.glGetFloatv(gl.GL_PROJECTION_MATRIX, projectionMatrix.ptr)
+	modelViewProjectionMatrix:mul(projectionMatrix, modelViewMatrix)
 
 	gl.glPointSize(2)
+glreport'here'	
+
 	displayShader:use()
+glreport'here'	
+	gl.glUniformMatrix4fv(displayShader.uniforms.modelViewProjectionMatrix.loc, 1, false, modelViewProjectionMatrix.ptr)
+glreport'here'	
+	
+	posTexs:cur():bind(0)
+--glreport'here'	
+	velTexs:cur():bind(1)
+--glreport'here'	
+	gradTex:bind(2)
 
-	gl.glEnableClientState(gl.GL_VERTEX_ARRAY)
-	gl.glBindBuffer(gl.GL_ARRAY_BUFFER, vbo.id)
-	gl.glVertexPointer(4, gl.GL_FLOAT, 0, nil)
-	gl.glBindBuffer(gl.GL_ARRAY_BUFFER, 0)
-	gl.glDrawArrays(gl.GL_POINTS, 0, fieldDim*fieldDim)
-	gl.glDisableClientState(gl.GL_COLOR_ARRAY)
-
-	gl.glUseProgram(0)
-	gl.glPointSize(1)
+--[[
+	uvBuf:bind()
+	gl.glEnableVertexAttribArray(0)
+	--displayShader.vao:bind()
+	gl.glDrawArrays(gl.GL_POINTS, 0, count)
+	gl.glDisableVertexAttribArray(0)
+	uvBuf:unbind()
 --]]
+-- [[
+	uvBuf:bind()
+glreport'here'	
+	gl.glEnableClientState(gl.GL_VERTEX_ARRAY)
+glreport'here'	
+--	displayShader.vao:bind()
+glreport'here'	
+	gl.glVertexPointer(2, gl.GL_FLOAT, ffi.sizeof'vec2f_t', nil)
+glreport'here'	
+	gl.glDrawArrays(gl.GL_POINTS, 0, count)
+glreport'here'	
+--	displayShader.vao:unbind()
+glreport'here'	
+	gl.glDisableClientState(gl.GL_VERTEX_ARRAY)
+glreport'here'	
+	uvBuf:unbind()
+glreport'here'	
+--]]
+--[[
+glreport'here'	
+	gl.glBegin(gl.GL_POINTS)
+	for i=0,count-1 do
+		gl.glVertex2f(uvCPUMem[0 + 2 * i], uvCPUMem[1 + 2 * i])
+	end
+	gl.glEnd()
+glreport'here'	
+--]]
+
+	GLTex1D:unbind(2)
+	GLTex2D:unbind(1)
+--glreport'here'	
+	GLTex2D:unbind(0)
+--glreport'here'	
+
+	displayShader:useNone()
+glreport'here'	
+	gl.glPointSize(1)
+glreport'here'	
 
 
 
@@ -338,10 +528,10 @@ function App:update(...)
 	gl.glDisable(gl.GL_BLEND)
 	--]=]
 
-	gl.glDisable(gl.GL_TEXTURE_2D)
-
-	-- gui update & view update
-	App.super.update(self, ...)
+	-- super does the view setup.  I've already done it earlier.
+	--App.super.update(self, ...)
+	-- so instead I'll call super.super
+	App.super.super.update(self, ...)
 
 	-- fps counter
 	self.frame = (self.frame or 0) + 1
